09d1b1acc6af6341cef5a3ec360b9638530bacef
Abhilash <Abhilash@Abhilash>
2018-04-22 22:36:40 +0530

    First Commit

---
+++ Spring/src/com/SpringDemo/DAO/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.SpringDemo.DAO;
+
+import java.util.List;
+
+import com.SpringDemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
